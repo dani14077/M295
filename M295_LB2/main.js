3ee57baee6c48425df6ebe432b01bc6a9e9ad52f
dani14077 <dtoboulter@gmail.com>
2023-06-13 13:57:44 +0200

    Working post method

--- M295_LB2/main.js
+++ M295_LB2/main.js
@@ -6,15 +6,15 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
 let tasks = [
-    {id: 1, title: "Call Client", description: "Schedule a phone call with the client to discuss project updates", done: false, due: new Date("2023-06-15").toLocaleDateString("de-CH")},
-    {id: 2, title: "Send Meeting Agenda", description: "Email the meeting agenda to all participants before the scheduled team meeting.", done: false, due: new Date("2023-06-17").toLocaleDateString("de-CH")},
-    {id: 3, title: "Update Sales Spreadsheet", description: "Enter the latest sales data into the spreadsheet and calculate total revenue.", done: false, due: new Date("2023-06-20").toLocaleDateString("de-CH")},
-    {id: 4, title: "Review Website Content", description: "Proofread and review the content on the company's website for any errors or outdated information.", done: false, due: new Date("2023-06-22").toLocaleDateString("de-CH")},
-    {id: 5, title: "Purchase Office Supplies", description: "Place an order for necessary office supplies like stationery and printer ink.", done: false, due: new Date("2023-06-25").toLocaleDateString("de-CH")}
+    {id: 1, title: "Call Client", description: "Schedule a phone call with the client to discuss project updates", done: false, dueDate: new Date("2023-06-15").toLocaleDateString("de-CH")},
+    {id: 2, title: "Send Meeting Agenda", description: "Email the meeting agenda to all participants before the scheduled team meeting.", done: false, dueDate: new Date("2023-06-17").toLocaleDateString("de-CH")},
+    {id: 3, title: "Update Sales Spreadsheet", description: "Enter the latest sales data into the spreadsheet and calculate total revenue.", done: false, dueDate: new Date("2023-06-20").toLocaleDateString("de-CH")},
+    {id: 4, title: "Review Website Content", description: "Proofread and review the content on the company's website for any errors or outdated information.", done: false, dueDate: new Date("2023-06-22").toLocaleDateString("de-CH")},
+    {id: 5, title: "Purchase Office Supplies", description: "Place an order for necessary office supplies like stationery and printer ink.", done: false, dueDate: new Date("2023-06-25").toLocaleDateString("de-CH")}
 ]
 
-app.get('/tasks', (require, response) => {
-    response.json(tasks).status(200)
+app.get('/tasks', (req, res) => {
+    res.json(tasks).status(200)
 });
 
 app.get('/tasks/:id', (req, res) => {
@@ -26,6 +26,18 @@ app.get('/tasks/:id', (req, res) => {
     } else {
         return res.status(404).send("Task not found.");
     }
+});
+
+app.post('/tasks', (req, res) => {
+    const newTask = {
+        id: tasks.length + 1,
+        title: req.body.title,
+        description: req.body.description,
+        done: false,
+        due: req.body.due
+    };
+    tasks.push(newTask);
+    res.status(201).json(newTask);
 });
 
 app.listen(port, () => {
