5d09bda8455b41a994065e2985095b3347ba71ce
dani14077 <dtoboulter@gmail.com>
2023-06-13 08:01:52 +0200

    ÜK Modul 295 Tag 4

--- Library1/main.js
+++ Library1/main.js
@@ -10,7 +10,11 @@ let books = [
     {isbn: 2, title: "The last Day on Earth", year: 2021, author: "Joe Biden"},
     {isbn: 3, title: "Success of Facebook", year: 2012, author: "Mark Zuckerberg"},
     {isbn: 4, title: "Sending my Tesla to the moon", year: 2020, author: "Elon Musk"},
-    {isbn: 5, title: "Workman is the worst keyboard layout", year: 2023, author: "Bill Gates"}
+    {isbn: 5, title: "Workman is the worst keyboard layout", year: 2023, author: "Bill Gates"},
+    {isbn: 6, title: "Why Swiss Education sucks", year: 2021, author: "Alain Berset"},
+    {isbn: 7, title: "My English is the best", year: 2017, author: "Jörg Jaggi"},
+    {isbn: 8, title: "How to boss everyone and not follow the rules", year: 2022, author: "Adrian Gfeller"},
+    {isbn: 9, title: "Apple is better than Samsung", year: 2010, author: "Steve Jobs"}
 ];
 
 let lends = [
@@ -72,27 +76,73 @@ app.delete('/books/:isbn', (req, res) => {
 });
 
 
+// Get all lends
 app.get('/lends', (req, res) => {
-    res.json(lends)
+  res.json(lends);
 });
 
+// Get a specific lend by ID
 app.get('/lends/:id', (req, res) => {
-    const id = req.params.id;
-    const lend = lends.find((lend) => lend.id === parseInt(id));
+  const lendId = req.params.id;
+  const lend = lends.find((l) => l.id === parseInt(lendId));
 
-    if (lend) {
-        return res.json(lend);
-    } else {
-        return res.status(404).send("Lend not found.");
-    }
+  if (lend) {
+    res.json(lend);
+  } else {
+    res.status(404).send('Lend not found.');
+  }
 });
 
+// Create a new lend
 app.post('/lends', (req, res) => {
+  const lendData = req.body;
+  const book = books.find((b) => b.isbn === lendData.isbn);
 
+  if (!book) {
+    res.status(404).json({ error: 'Book not found' });
+    return;
+  }
+
+  const isBookAlreadyLent = lends.some((l) => l.isbn === lendData.isbn && !l.returned_at);
+  if (isBookAlreadyLent) {
+    res.status(422).json({ error: 'The book is already borrowed' });
+    return;
+  }
+
+  const customerLendsCount = lends.filter((l) => l.customer_id === lendData.customer_id && !l.returned_at).length;
+  if (customerLendsCount >= 3) {
+    res.status(422).json({ error: 'The customer already has 3 borrowed books' });
+    return;
+  }
+
+  const newLend = {
+    id: lends.length + 1,
+    customer_id: lendData.customer_id,
+    isbn: lendData.isbn,
+    borrowed_at: new Date().toLocaleDateString('de-CH'),
+    returned_at: null,
+  };
+
+  lends.push(newLend);
+  res.status(201).json(newLend);
 });
 
+// Update a lend by ID
 app.patch('/lends/:id', (req, res) => {
+  const lendId = req.params.id;
+  const lendUpdates = req.body;
+  const lend = lends.find((l) => l.id === parseInt(lendId));
 
+  if (!lend) {
+    res.status(404).json({ error: 'Lend not found' });
+    return;
+  }
+
+  lend.customer_id = lendUpdates.customer_id || lend.customer_id;
+  lend.isbn = lendUpdates.isbn || lend.isbn;
+  lend.returned_at = lendUpdates.returned_at || lend.returned_at;
+
+  res.json(lend);
 });
 
 app.listen(port, () => {
---
+++ session1/main.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const session = require('express-session')
+const app = express()
+const port = 3000
+
+app.use(session({
+    secret: 'supersecret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {}
+}));
+
+app.get('/', function (request, response, _) {
+    
+    request.session.views = (request.session.views | 0) + 1
+    console.log(request.session)
+    response.end(request.session.views + ' views')
+});
+app.get('/name', (request, response) => {
+    const name = request.session.name;
+
+    if (name) {
+        response.send(name);
+    } else {
+        response.sendStatus(404);
+    }
+});
+
+app.post('/name', (request, response) => {
+    const name = request.query.name;
+
+    request.session.name = name
+
+    response.sendStatus(200);
+});
+
+app.delete('/name', (request, response) => {
+    delete request.session.name;
+    response.sendStatus(204);
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
